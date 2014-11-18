a1babd2c9bdfdd03bf944a2a99d42e98b54cb746
Nichlos Incardone <nickincardone@gmail.com>
2014-11-17 19:29:48 -0500

    added skeleton to python progress bar for telling time remaining

--- python/ProgressBar.py
+++ python/ProgressBar.py
@@ -1,5 +1,5 @@
 import sys
-
+import datetime
 # for example only
 import time
 
@@ -10,7 +10,9 @@ class ProgressBar(object):
     def __init__(self, total):
         self.total = total
         self.cur = 0
-        self.bar_size = 40
+        self.bar_size = 50
+        #self.start_time = datetime.datetime.now()
+        self.percent = 999
         self.draw_bar()
 
     def update(self, amount=1):
@@ -27,12 +29,24 @@ class ProgressBar(object):
         percent = float(self.cur) / self.total
         hashes = '#' * int(round(percent * self.bar_size))
         spaces = ' ' * (self.bar_size - len(hashes))
-        sys.stdout.write("\rPercent: [{0}] {1}%".format(
-            hashes + spaces, int(round(percent * 100))))
-        sys.stdout.flush()
+
+        #time_diff = datetime.datetime.now() - self.start_time
+        #if not percent == 0: 
+        #    est_time_remaining = time_diff.total_seconds() / percent - time_diff.total_seconds()
+        #else:
+        #    est_time_remaining = 60
+        #mins_remaining = divmod(est_time_remaining, 60)[0]
+        #sys.stdout.write("\rPercent: [{0}] {1}% {2} Minutes Left".format(
+        #    hashes + spaces, int(round(percent * 100)), mins_remaining))
+
+        if percent != self.percent:
+            sys.stdout.write("\rPercent: [{0}] {1}%".format(
+                hashes + spaces, int(round(percent * 100))))
+            sys.stdout.flush()
+        self.percent = percent
 
 if __name__ == "__main__":
     b = ProgressBar(100)
     for x in range(100):
         b.update()
-        time.sleep(.2)
+        time.sleep(.5)
