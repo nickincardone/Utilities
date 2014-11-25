b8aa460117dcd598ede6940706f18d0ee7471966
Nichlos Incardone <nickincardone@gmail.com>
2014-11-24 21:58:16 -0500

    updated java readme and removed excess imports in java excel helper

--- java/ExcelHelper/ExcelHelper.java
+++ java/ExcelHelper/ExcelHelper.java
@@ -2,8 +2,6 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
-
-
 import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -17,10 +15,6 @@ import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.ss.util.AreaReference;
 import org.apache.poi.ss.util.CellReference;
 
-//import java.io.BufferedReader;
-//import java.io.FileNotFoundException;
-//import java.io.FileReader;
-//import java.io.IOException;
 
 public class ExcelHelper {
 	public static void main(String[] args) {
--- java/README.md
+++ java/README.md
@@ -3,3 +3,8 @@
 ### Bloom Filter
 Implementation of a bloom filter I did for CS 4540
 Read more about a bloom filter here: http://en.wikipedia.org/wiki/Bloom_filter
+
+### Excel Helper
+Some code that reads and evaluates excel files and then writes back to them
+Includes custom implementation of the Apache POI CellRange interface and a copy paste 
+function with a cell range
